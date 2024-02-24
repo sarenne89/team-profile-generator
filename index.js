8b00bc04ffcbc3955ade3839ac3f9341dc68b98a
Sarenne89 <mdunmore89@gmail.com>
2024-02-24 23:04:09 +0000

    added more questions for employee generation

--- index.js
+++ index.js
@@ -15,32 +15,116 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 //questions to gather information on the team
-const managerQuestions = () => {
+const createManager = () => {
     return inquirer.prompt ([
         {
             type: "input",
-            name: "name",
+            name: "managerName",
             message: "What is the Manager's name?",
         },
         {
             type: "input",
-            name: "ID",
+            name: "managerID",
             message: "What is the Manager's ID?",
         },
         {
             type: "input",
-            name: "Email",
+            name: "managerEmail",
             message: "What is the Manager's email address?",
         },
         {
             type: "input",
-            name: "Office",
+            name: "managerOffice",
             message: "What is the Manager's office number?",
         }
     ]).then(answers  => {
-    const manager = new Manager(answers.name, answers.ID, answers.Email, answers.Office)
+    const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOffice)
     devTeam.push(manager)
-    })}
+    createEmployee()
+    })
+};
+
+const createEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: "list",
+            name: "menu",
+            message: "Please select the next employee you could like to create:",
+            choices: ["Engineer", "Intern", "Team Complete"]
+        }
+    ]).then(userChoice => {
+        switch(userChoice.menu) {
+            case "Engineer":
+                createEngineer();
+                break;
+                
+            case "Intern":
+                createIntern();
+                break;
+
+            case "Team Complete":
+                buildTeam();
+        }
+    })
+};
+
+const createEngineer = () => {
+    return inquirer.prompt ([
+        {
+            type: "input",
+            name: "engineerName", 
+            message: "What is the engineer's name?",
+        },
+        {
+            type: "input",
+            name: "engineerID", 
+            message: "What is the engineer's ID number?",
+        },
+        {
+            type: "input",
+            name: "engineerEmail", 
+            message: "What is the engineer's email address?",
+        },
+        {
+            type: "input",
+            name: "engineerGithub", 
+            message: "What is the engineers Github username?",
+        },
+    ]).then(answers => {
+        const engineer = new Engineer (answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub)
+        devTeam.push(engineer)
+        createEmployee()
+    })
+}
+
+const createIntern = () => {
+    return inquirer.prompt ([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?",
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "What is the intern's ID number?",
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email address?",
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What school does the intern attend?",
+        },
+    ]).then(answers => {
+        const intern = new Intern (answers.internName, answers.internID, answers.internEmail, answers.internSchool)
+        devTeam.push(intern)
+        createEmployee()
+    })
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -49,7 +133,7 @@ function writeToFile(fileName, data) {
 
 // function to initialise questions
 function init() {
-    inquirer.prompt(questions).then((responses) => {
+    inquirer.prompt(createManager).then((responses) => {
         console.log("Creating your team!");
         writeToFile("./ouput/team.html", generateTeam({...responses}));
     });
